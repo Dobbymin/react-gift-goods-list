e15ef6037340478f4e447a555181deb7b680b472
Dobbymin <gmin0701@knu.ac.kr>
2024-07-09 21:03:43 +0900

    init: vite 환경 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,18 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	define: {
-		global: {},
-	},
-	plugins: [react()],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-		},
-	},
-	server: {
-		hmr: {
-			overlay: false,
-		},
-	},
+  define: {
+    global: {},
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    hmr: {
+      overlay: false,
+    },
+  },
 });
